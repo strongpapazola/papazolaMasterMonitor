aca4411e4f4d27dfa1151740246fe84163eaf171
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-05-09 22:47:55 +0700

    Update index.php

--- nodePackage/papazolaMasterMonitor/index.php
+++ nodePackage/papazolaMasterMonitor/index.php
@@ -1,14 +1,7 @@
 <?php
 
-function checkfile() {
-$a = shell_exec('cat check.json');
-$a = explode("\n",$a)[0];
-$a = json_decode($a, True)['run'];
-return $a;
-}
-
 $key = $_GET['key'];
-if (isset($key) and $key == 'b3af409bb8423187c75e6c7f5b683908') {
+if (isset($key) and $key == 'Y29iYQ==') {
 	echo shell_exec('cat result.json');
 } else {
 	echo '{"msg":"error code"}';
